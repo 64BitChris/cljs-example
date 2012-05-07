707ae352952e8e21f90db9f353d4ed83ac6745f7
Chris Shellenbarger <chris.shellenbarger@issinc.com>
2012-05-07 17:13:29 -0600

    Added CLJS Support

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
 (defproject cljs-example "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
-            :main cljs-example.server)
+  :description "Example for using ClojureScript in a web app"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.2.1"]]
+  ;lein-cljsbuild script fixed by http://bit.ly/JbZLr0
+  :plugins [[lein-cljsbuild "0.1.8"]]
+  :cljsbuild {
+               :builds [{
+                          ; The path to the top-level ClojureScript source directory:
+                          :source-path "src-cljs"
+                          ; The standard ClojureScript compiler options:
+                          ; (See the ClojureScript compiler documentation for details.)
+                          :compiler {
+                                      :output-to "resources/public/js/main.js" ; default: main.js in current directory
+                                      :optimizations :whitespace
+                                      :pretty-print true}}]
+               }
+  :hooks [leiningen.cljsbuild]
+  :main cljs-example.server)
 
